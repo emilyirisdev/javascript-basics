3f932eba55d3fa71e9c322472148d5c425f57bbd
emilyirisdev <93999129+emilyirisdev@users.noreply.github.com>
2021-12-02 16:46:26 -0500

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,22 @@
+/*
+
+const s = ‘Hello World!’;
+
+console.log(s.length);
+
+console.log(s.toUpperCase());  -> parentheses required for method (function of an object)
+
+console.log(s.toLowerCase()); 
+
+console.log(s.substring(0, 5));  -> takes in to indexes, start (at 0) and end.  This results in Hello from ‘Hello World’
+
+console.log(s.substring(0, 5).toUpperCase());  -> produces HELLO
+
+console.log(s.split(‘ ‘));  -> splits each character into array
+
+Other Example of split
+
+const s = ‘computers, technology, IT, code’;
+console.log(s.split(‘, ‘));  -> splits each word in const s into array since they are separated by “, “
+
+*/
