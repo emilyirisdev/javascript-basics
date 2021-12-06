fa6294c7702c70ee270a2742e11689990d46adb3
emilyirisdev <93999129+emilyirisdev@users.noreply.github.com>
2021-12-06 15:25:05 -0500

    Update and rename loop2 to loop2.js

---
+++ loop2.js
@@ -0,0 +1,167 @@
+/* LOOPS
+
+FOR
+
+for (let i = 0; i < 10; i++) {
+console.log(i);
+}
+
+	/* let i = 0 iterator
+	   i < 10 condition
+	   i++ increments */
+
+for (let i = 0; i < 10; i++) {
+console.log(`For Loop Number: ${i}`);
+}
+
+For Loop Number: 0
+For Loop Number: 1
+For Loop Number: 2
+For Loop Number: 3
+For Loop Number: 4
+For Loop Number: 5
+For Loop Number: 6
+For Loop Number: 7
+For Loop Number: 8
+For Loop Number: 9
+
+WHILE
+
+let i = 0;
+while(i < 10) {
+console.log(`While Loop Number: ${i}`);
+i++;
+}
+
+While Loop Number: 0
+While Loop Number: 1
+While Loop Number: 2
+While Loop Number: 3
+While Loop Number: 4
+While Loop Number: 5
+While Loop Number: 6
+While Loop Number: 7
+While Loop Number: 8
+While Loop Number: 9
+
+LOOPING AN ARRAY
+
+for (let i = 0; i < todos.length; i++) {
+console.log(todos[i].text);
+}
+
+Take out trash
+Meeting with boss
+Dentist appt
+
+FOR OF LOOP
+
+for(let todo of todos) {
+console.log(todo);
+}
+
+{id: 1 text: “Take out trash”, isCompleted: true}
+{id: 2 text: “Meeting with boss”, isCompleted: true}
+{id: 3 text: “Dentist appt”, isCompleted: false}
+
+for(let todo of todos) {
+console.log(todo.text);
+}
+
+Take out trash
+Meeting with boss
+Dentist appt
+
+for(let todo of todos) {
+console.log(todo.id);
+}
+
+1
+2
+3
+
+/* LOOPS
+
+FOR
+
+for (let i = 0; i < 10; i++) {
+console.log(i);
+}
+
+	/* let i = 0 iterator
+	   i < 10 condition
+	   i++ increments */
+
+for (let i = 0; i < 10; i++) {
+console.log(`For Loop Number: ${i}`);
+}
+
+For Loop Number: 0
+For Loop Number: 1
+For Loop Number: 2
+For Loop Number: 3
+For Loop Number: 4
+For Loop Number: 5
+For Loop Number: 6
+For Loop Number: 7
+For Loop Number: 8
+For Loop Number: 9
+
+WHILE
+
+let i = 0;
+while(i < 10) {
+console.log(`While Loop Number: ${i}`);
+i++;
+}
+
+While Loop Number: 0
+While Loop Number: 1
+While Loop Number: 2
+While Loop Number: 3
+While Loop Number: 4
+While Loop Number: 5
+While Loop Number: 6
+While Loop Number: 7
+While Loop Number: 8
+While Loop Number: 9
+
+LOOPING AN ARRAY
+
+for (let i = 0; i < todos.length; i++) {
+console.log(todos[i].text);
+}
+
+Take out trash
+Meeting with boss
+Dentist appt
+
+FOR OF LOOP
+
+for(let todo of todos) {
+console.log(todo);
+}
+
+{id: 1 text: “Take out trash”, isCompleted: true}
+{id: 2 text: “Meeting with boss”, isCompleted: true}
+{id: 3 text: “Dentist appt”, isCompleted: false}
+
+for(let todo of todos) {
+console.log(todo.text);
+}
+
+Take out trash
+Meeting with boss
+Dentist appt
+
+for(let todo of todos) {
+console.log(todo.id);
+}
+
+1
+2
+3
+
+HIGH ORDER ARRAY METHODS (RECOMMENDED)
+
+
