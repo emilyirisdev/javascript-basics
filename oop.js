dea0b47be06177aad1fb57b95d46f1bb1593258e
emilyirisdev <93999129+emilyirisdev@users.noreply.github.com>
2021-12-13 05:59:28 -0500

    Update oop.js

--- oop.js
+++ oop.js
@@ -1,5 +1,21 @@
-/* OOP - OBEJECT ORIENTED PROGRAMMING
+/*OBJECT ORIENTED PROGRAMMING OOP
 
+CONSTRUCTOR FUNCTION (function name starts with capital letter, for parameters pass in properties you want to set “firstName”, set the parameters to properties of the object by using “this” )
 
+function Person(firstName, LastName, dob) {
+this.firstName = firstName;
+this.lastName = lastName;
+this.dob = dob
+}
+
+// Instantiate an object
+const person1 = new Person(‘John’, ‘Doe’, ‘4/3/80’);
+console.log(person1)
+
+Person {firstName: “John”, lastName: “Doe”, dob: “4/3/80”}
+dob: “4/3/80”
+firstName: “John”
+lastName: “Doe”
+_proto_: Object
 
 */
